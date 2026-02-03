b92a28b662cf815c051a20b8bc7d53396cf4db2f
Cinereo <greg@cinereo.it>
2026-02-03 12:42:06 +0100

    Fix Astro Vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   site: 'https://amalfi.day',
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   integrations: [mdx()],
 })
