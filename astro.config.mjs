779533c9d15b5e12b2494c835907472a01d202d9
Cinereo <greg@cinereo.it>
2026-02-09 19:39:36 +0100

    Add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://amalfi.day',
   output: 'static',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   vite: {
     server: {
       proxy: {
