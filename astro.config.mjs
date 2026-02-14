c59ef1ba0fda2565a489dd159b299f6cfc91dbbe
Cinereo <greg@cinereo.it>
2026-02-14 20:21:12 +0100

    SEO updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,18 @@ import sitemap from '@astrojs/sitemap'
 export default defineConfig({
   site: 'https://amalfi.day',
   output: 'static',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) =>
+        !page.includes('/edit/') &&
+        !page.includes('/design-guidelines') &&
+        !page.includes('/photoshootings/template') &&
+        !page.includes('/parking-in-amalfi-coast-2025') &&
+        !page.includes('/beaches-in-amalfi') &&
+        !page.includes('/amalfi-coast-on-the-moto'),
+    }),
+  ],
   vite: {
     server: {
       proxy: {
